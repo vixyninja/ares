17c28e149313d893cec69a0cedd0c906771eee3e
Huỳnh Hồng Vỹ <hhvy2003.dev@gmail.com>
2025-10-19 23:04:58 +0700

    feat(portfolio): update dependencies, enhance styling with Tailwind CSS, and refactor layout structure

--- apps/portfolio/app/page.tsx
+++ apps/portfolio/app/page.tsx
@@ -1,81 +1,82 @@
-import Image, { type ImageProps } from 'next/image';
-import { Button } from '@ares/ui/button';
-import styles from './page.module.css';
-
-type Props = Omit<ImageProps, 'src'> & {
-  srcLight: string;
-  srcDark: string;
-};
-
-const ThemeImage = (props: Props) => {
-  const { srcLight, srcDark, ...rest } = props;
-
-  return (
-    <>
-      <Image {...rest} src={srcLight} className="imgLight" />
-      <Image {...rest} src={srcDark} className="imgDark" />
-    </>
-  );
-};
+import Link from "next/link";
+import React from "react";
+import styles from "./page.module.css";
 
 export default function Home() {
   return (
-    <div className={styles.page}>
-      <main className={styles.main}>
-        <ThemeImage
-          className={styles.logo}
-          srcLight="turborepo-dark.svg"
-          srcDark="turborepo-light.svg"
-          alt="Turborepo logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol>
-          <li>
-            Get started by editing <code>apps/web/app/page.tsx</code>
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+    <React.Fragment>
+      <div className="mx-auto flex h-screen w-4xl flex-row">
+        <div className="mx-auto flex h-full w-3/4 max-w-7xl flex-row">
+          {/* Double Vertical Line */}
+          <div className="flex flex-row gap-12">
+            <div className="h-full w-10 border-r border-l border-white bg-[#48A860] opacity-25"></div>
+          </div>
+          <div className="flex h-full flex-col overflow-y-auto px-4 py-2">
+            {/* Header */}
+            <header className="mx-auto flex h-16 w-full">
+              {/* Logo */}
+              <div className="flex items-center">
+                <span className="text-3xl font-bold text-white">Portfoli</span>
+                <span className="text-3xl font-bold text-[#48A860]">o</span>
+              </div>
+            </header>
 
-        <div className={styles.ctas}>
-          <a
-            className={styles.primary}
-            href="https://vercel.com/new/clone?demo-description=Learn+to+implement+a+monorepo+with+a+two+Next.js+sites+that+has+installed+three+local+packages.&demo-image=%2F%2Fimages.ctfassets.net%2Fe5382hct74si%2F4K8ZISWAzJ8X1504ca0zmC%2F0b21a1c6246add355e55816278ef54bc%2FBasic.png&demo-title=Monorepo+with+Turborepo&demo-url=https%3A%2F%2Fexamples-basic-web.vercel.sh%2F&from=templates&project-name=Monorepo+with+Turborepo&repository-name=monorepo-turborepo&repository-url=https%3A%2F%2Fgithub.com%2Fvercel%2Fturborepo%2Ftree%2Fmain%2Fexamples%2Fbasic&root-directory=apps%2Fdocs&skippable-integrations=1&teamSlug=vercel&utm_source=create-turbo"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image className={styles.logo} src="/vercel.svg" alt="Vercel logomark" width={20} height={20} />
-            Deploy now
-          </a>
-          <a
-            href="https://turborepo.com/docs?utm_source"
-            target="_blank"
-            rel="noopener noreferrer"
-            className={styles.secondary}
-          >
-            Read our docs
-          </a>
+            {/* About */}
+            <section id="about">
+              <p className={styles.cool}>
+                <span data-text="Hi,">Hi,</span>
+                <span data-text=" &nbsp;I'm"> &nbsp;I&apos;m</span>
+                <span data-text=" &nbsp;Vy."> &nbsp;Vy.</span>
+                <br />
+                <span data-text="Nice">Nice</span>
+                <span data-text=" &nbsp;to"> &nbsp;to</span>
+                <span data-text=" &nbsp;meet"> &nbsp;meet</span>
+                <span data-text=" &nbsp;you!"> &nbsp;you!</span>
+              </p>
+              <br />
+              <p className="text-white">
+                I&apos;m <b>Huynh Hong Vy</b>, a software engineer with a
+                passion for building backend systems. I previously worked at
+                <a href="https://cozwork.com/vi/" className="text-white">
+                  &nbsp;
+                  <b>Cozwork JSC</b> as a Backend Developer. I&apos;m now open
+                  to new opportunities and available for hire.
+                </a>
+              </p>
+            </section>
+
+            {/* Skills */}
+            <section id="skills"></section>
+
+            {/* Projects */}
+            <section id="projects"></section>
+
+            {/* Contact */}
+            <section id="contact"></section>
+
+            {/* Footer */}
+            <footer id="footer"></footer>
+          </div>
         </div>
-        <Button appName="web" className={styles.secondary}>
-          Open alert
-        </Button>
-      </main>
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com/templates?search=turborepo&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image aria-hidden src="/window.svg" alt="Window icon" width={16} height={16} />
-          Examples
-        </a>
-        <a href="https://turborepo.com?utm_source=create-turbo" target="_blank" rel="noopener noreferrer">
-          <Image aria-hidden src="/globe.svg" alt="Globe icon" width={16} height={16} />
-          Go to turborepo.com →
-        </a>
-      </footer>
-    </div>
+
+        {/* Navigation */}
+        <div className="flex h-full w-1/4 flex-col justify-center border-r border-white bg-red-950">
+          <nav className="flex flex-col items-center gap-4">
+            <Link href="#about" className="text-white">
+              <span className="text-white">About</span>
+            </Link>
+            <Link href="#skills" className="text-white">
+              <span className="text-white">Skills</span>
+            </Link>
+            <Link href="#projects" className="text-white">
+              <span className="text-white">Projects</span>
+            </Link>
+            <Link href="#contact" className="text-white">
+              <span className="text-white">Contact</span>
+            </Link>
+          </nav>
+        </div>
+      </div>
+    </React.Fragment>
   );
 }
-
