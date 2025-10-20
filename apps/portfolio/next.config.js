8ee4f297c9615bfce728269a24683baf9d3b9028
Huỳnh Hồng Vỹ <hhvy2003.dev@gmail.com>
2025-10-20 12:31:38 +0700

    chore(portfolio): update Next.js configuration with basePath and assetPrefix

--- apps/portfolio/next.config.js
+++ apps/portfolio/next.config.js
@@ -1,9 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: "export",
-  images: {
-    unoptimized: true,
-  },
+  basePath: "/portfolio",
+  assetPrefix: "/portfolio/",
+  images: { unoptimized: true },
 };
 
 export default nextConfig;
